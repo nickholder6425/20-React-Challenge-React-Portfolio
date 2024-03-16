23d817b910880f81eb30694cc96f2107099f5b36
nicholasholder <ngholder@hotmail.com>
2024-03-15 21:59:15 -0500

    modified files

---
+++ src/components/Footer/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { IconContext } from "react-icons/lib";
+import { FaGithub, FaEnvelope, FaLinkedin, FaStackOverflow } from "react-icons/fa";
+import { Container, Row, Col } from "react-bootstrap";
+
+function Footer() {
+  return (
+    <Container >
+    <IconContext.Provider value={{ size:"3em"}}>
+    <footer>
+      <Row>
+      <Col md={{ span: 4, offset: 10 }} style={{
+        width: "20rem", marginBottom: "5rem"
+      }}>
+        <a href="mailto:njthanhtrang@gmail.com">
+          <FaEnvelope />
+        </a>
+        <a href="https://github.com/njthanhtrang" target="_blank" rel="noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/njthanhtrang/" target="_blank" rel="noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://stackoverflow.com/users/story/15040375" target="_blank" rel="noreferrer">
+          <FaStackOverflow />
+        </a>
+        </Col>
+      </Row>
+    </footer>
+    </IconContext.Provider>
+    </Container>
+    
+  );
+
+}
+
+export default Footer;
