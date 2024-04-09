d89ec2e41f35aaf41321570f43181228209906b4
nicholasholder <ngholder@hotmail.com>
2024-04-09 16:59:16 -0500

    modified Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ export default function Footer() {
         <h2></h2>
         <hr />
         <div className="contacts">
-            <a href="https://github.com/AndrueGage" target="_blank" className="animate">
+            <a href="https://github.com/nickholder6425" target="_blank" className="animate">
                 <img src="/images/github.svg" alt="github icon" />Github
             </a>
-            <a href="mailto:trainer.andrue@gmail.com" target="_blank" className="animate">
+            <a href="mailto:ngholder@hotmail.com " target="_blank" className="animate">
                 <img src="/images/email.svg" alt="email icon" />Email
             </a>
-            <a href="https://www.linkedin.com/in/andrue-desmarais-1007b82a7/" target="_blank" className="animate">
+            <a href="https://www.linkedin.com/in/nick-holder-570110272/" target="_blank" className="animate">
                 <img src="/images/linkedin.svg" alt="linkedin icon" />LinkedIn
             </a>
         </div>
