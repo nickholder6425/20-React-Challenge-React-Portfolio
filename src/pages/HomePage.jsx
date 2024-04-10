e87dc7a44138224e20a002fcfd9e9040c290f383
nicholasholder <ngholder@hotmail.com>
2024-04-09 20:28:13 -0500

    modified files

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,8 +3,8 @@ export default function HomePage(){
         <section id="about">
             <h2>About Me</h2>
             <hr />
-            <p>My name is Nick, and I am a full stack developer from Nashville, TN.<br />
-            I am a purposeful professional with six years of experience as a successful dog trainer, showcasing a proven track record of managing a business with 30+ employees. Leveraging a deep understanding of learning theory, consistently achieved exceptional results in canine behavior modification. Eager to transition into the dynamic field of Information Technology, currently enrolled in a Full Stack Developer Bootcamp at Vanderbilt University, demonstrating a commitment to enhancing the technical skills required for this industry. Possessing a unique blend of leadership, problem-solving, and adaptability, poised to bring a fresh perspective and a strong work ethic to the IT sector, capitalizing on my diverse background to drive success in new and challenging environments.
+            <p>My name is Nick, and I'm an aspirering Full Stack Developer from Nashville, TN.<br />
+            A results-oriented Supply Chain professional with extensive experience in Aerospace, Merchandise, and Beverage industries, possessing a diverse skillset encompassing logistics, transportation, and operations management alongside newly acquired Full Stack Web Development proficiency. With a Bachelor of Science in Liberal Studies and completion of a Full Stack Web Development program, I bring a blend of frontend and backend technologies, including HTML, CSS, JavaScript, React.js, Node.js, MongoDB, and MySQL. My background ranges from procurement and supply chain initiatives where I led projects resulting in significant cost reductions to roles as a professional driver at Ruan and PepsiCo where I streamlined operations. Now seeking to apply my strategic mindset, analytical skills, and commitment to innovation to the field of Full Stack Web Development, I am poised to contribute to dynamic teams and drive impactful digital solutions.
             </p>
         </section>
     )
